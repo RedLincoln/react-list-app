d24e6c0f17a751ae8df93327d59844426324e064
Red <red@linux-sxoy.home>
2019-12-15 12:19:24 +0000

    Note Item update

--- src/components/NoteDetail/NoteDetail.tsx
+++ src/components/NoteDetail/NoteDetail.tsx
@@ -28,18 +28,13 @@ export const NoteDetail: React.FC<Props> = ( { match } ) => {
 		items !== undefined && ListService.updateNoteItems(+match.params.noteId, items);
 	}, [items])
 
-	const toggleNewElement = () => {
-		document.getElementById('new-button')?.classList.toggle('hidden')	
-		document.getElementById('new-input-text')?.classList.toggle('hidden')	
-	}
 
 	const onInputTextEnter = (event: React.KeyboardEvent) => {
-		if (event.key === 'Enter') {
-			const item = document.querySelector('#new-input-text > input[type=text]')
-			if (item instanceof HTMLInputElement){
-				items !== undefined && setItems([...items, item.value])
-				toggleNewElement()
-			}
+		const input = event.target
+		const partner = document.getElementById('new-input-text')
+		if (event.key === 'Enter' && input && input instanceof HTMLInputElement && items && partner instanceof HTMLElement) {
+			setItems([...items, input.value])
+			togglePartners(partner)
 		}
 	}
 	
@@ -54,7 +49,30 @@ export const NoteDetail: React.FC<Props> = ( { match } ) => {
 		}
 	}
 
+	const togglePartners = (primaryElement: HTMLElement) => {
+		if (primaryElement.dataset.partnerId) {
+			document.getElementById(primaryElement.dataset.partnerId)?.classList.toggle('hidden')
+			primaryElement.classList.toggle('hidden')
+		}
+	}
 
+	const toggleOnClick = (event: React.MouseEvent) => {
+		const element = event.target;
+		if (element instanceof HTMLElement){	
+			togglePartners(element)	
+		}
+	}
+
+	const updateItem = (event: React.KeyboardEvent) => {
+		const input = event.target;
+		const key = event.key;
+		if (key === 'Enter' && input instanceof HTMLInputElement && input.dataset.itemId && items){
+			items[+input.dataset.itemId] = input.value
+			setItems([...items])
+			togglePartners(input)
+		}
+	}
+	
 	return (
 		<div className="NoteDetail">
 			<h3>{ title }</h3>
@@ -62,13 +80,15 @@ export const NoteDetail: React.FC<Props> = ( { match } ) => {
 				{items?.map((item, index) => {
 				return (<div key={ index } className='note-item-list'>
 									<li>
-							 	 		{item}
+										<span onClick={ toggleOnClick } id={ `${index}Value` } data-partner-id={ `${index}Index` }>{item}</span>
+										<input id={ `${index}Index` } className='hidden' type='text' onKeyDown={ updateItem } 
+											data-partner-id={`${index}Value`} data-item-id={ index }/>
 									</li>
 									<button id="delete-button" onClick={ deleteItem } data-item-index={ index }>x</button>
 					 		 </div>)
 				})}
-				<button id='new-button' onClick={ toggleNewElement }>New</button>
-				<li id='new-input-text' className='hidden'><input type='text' onKeyDown={ onInputTextEnter } ></input></li>
+				<button id='new-button' onClick={ toggleOnClick } data-partner-id='new-input-text' >New</button>
+				<li id='new-input-text' className='hidden' data-partner-id='new-button'><input type='text' onKeyDown={ onInputTextEnter } /></li>
 			</ul>
 	</div>
 	)
